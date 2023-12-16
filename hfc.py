f7f7b41c467acd8c4057e72a9da2a99a20625d67
Jacob J Webber <jacobjwebber@gmail.com>
2023-12-16 01:09:33 +0000

    updated hfc to test

--- hfc.py
+++ hfc.py
@@ -4,28 +4,32 @@ import lightning as L
 
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
+from torch.optim import Adam
 
 
 # Local imports
-import network
 import dsp
+from hider import Hider
+from finder import Finder
+from combiner import Combiner
 
 class HFC(L.LightningModule):
     """ An implementation of the Hider Finder Combiner architecture for
     arbitrary control of speech signals """
-    def __init__(self, hp):
+    def __init__(self, hp, n_speakers):
         super(HFC, self).__init__()
 
         self.hp = hp
+        self.n_speakers = n_speakers
+        self.f0_bins = hp.control_variables.f0_bins
         self.automatic_optimization=False # required for manual optimization scheduling in lightning
 
         # Component networks
         # TODO rename width/size dichotomy also input/output mean same thing for hfc
         # TODO pass hp to get_[finder,hider,combiner] method and let it sort everything
-        self.hider = network.Hider(hp.h_rnn_size, hp.input_width, hp.hidden_size, hp.h_layers, dropout=hp.h_drop, denoising=hp.denoising)
-        self.finder = network.Finder(hp.f_rnn_size, hp.hidden_size, hp.cv_bins, hp.f_layers, dropout = hp.f_drop)
-        self.combiner = network.Combiner(hp.c_rnn_size, hp.hidden_size, hp.input_width, hp.c_layers, hp.cv_bins, dropout=hp.c_drop)
+        self.hider = Hider(hp.h_rnn_size, hp.input_width, hp.hidden_size, hp.h_layers, dropout=hp.h_drop, denoising=hp.denoising)
+        self.finder = Finder(hp.f_rnn_size, hp.hidden_size, n_speakers, hp.f_layers, dropout = hp.f_drop)
+        self.combiner = Combiner(hp.c_rnn_size, hp.hidden_size, hp.input_width, hp.c_layers, n_speakers, dropout=hp.c_drop)
 
         # The 'combiner loss' criterion
         self.g_criterion = nn.MSELoss()
@@ -35,45 +39,45 @@ class HFC(L.LightningModule):
     def configure_optimizers(self):
         # Define params that generate during synthesis as 'generator' (g)
         self.generator_params = list(self.hider.parameters()) + list(self.combiner.parameters())
-        self.g_opt = torch.optim.Adam(self.generator_params, lr=self.hp.g_lr, weight_decay=0.00)
-        self.f_opt = torch.optim.Adam(self.finder.parameters(), lr=self.hp.f_lr, weight_decay=0.00)
+        self.g_opt = Adam(self.generator_params, lr=self.hp.g_lr, weight_decay=0.00)
+        self.f_opt = Adam(self.finder.parameters(), lr=self.hp.f_lr, weight_decay=0.00)
         return [self.g_opt, self.f_opt]
 
 
-    def set_input(self, control_variable, f0, is_voiced, x):
+    def set_input(self, speaker_id, f0, is_voiced, mel):
 
         # Find the shape of the onehots for the cv
         batch_size, seq_len = f0.shape
-        hot_shape = (batch_size, seq_len, self.hp.cv_bins)
+        hot_shape = (batch_size, seq_len, self.f0_bins)
 
         # Two versions --- one matrix onehot and one array of indices
         #self.cv_index = dsp.onehot_index(control_variable, self.hp.cv_bins, self.hp.cv_range)
         #self.cv = dsp.onehotify(self.cv_index, hot_shape, self.hp.use_cuda)
-        print(control_variable.shape)
-        print(control_variable.type())
-        self.cv = control_variable
+        print(speaker_id.shape)
+        print(speaker_id.type())
+        self.speaker_id = speaker_id
 
-        self.f0_index = dsp.onehot_index(f0, self.hp.cv_bins, self.hp.f0_range)
+        self.f0_index = dsp.onehot_index(f0, self.f0_bins, self.hp.f0_range)
 
         self.f0 = dsp.onehotify(self.f0_index, hot_shape, self.hp.use_cuda)
         self.is_voiced = is_voiced.float().unsqueeze(2)
         # Call the input spectral features simply x --- dont want to tie features to name e.g. mel etc
-        self.x = x.float().transpose(1,2)
+        self.mel = mel.float().transpose(1,2)
 
 
     def forward(self):
         """Uses hider network to generate hidden representation"""
         self.hidden = self.hider(self.x)
-        self.controlled = self.combiner((self.hidden, self.cv, self.f0, self.is_voiced))
+        self.controlled = self.combiner((self.hidden, self.speaker_id, self.f0, self.is_voiced))
 
     def backward_F(self):
         # Attempt to predict the control variable from the hidden repr
-        pred_cv = self.finder(self.hidden.detach())
-        batch_size, seq_len, width = pred_cv.shape
+        pred_speaker_id = self.finder(self.hidden.detach())
+        batch_size, seq_len, width = pred_speaker_id.shape
         # Need to combine batch and sequence for CE loss
-        pred_cv = pred_cv.view(batch_size * seq_len, -1)
-        real_cv = self.cv_index.view(batch_size * seq_len).long()
-        self.finder_loss = self.f_criterion(pred_cv, real_cv)
+        pred_speaker_id = pred_speaker_id.view(batch_size * seq_len, -1)
+        real_speaker_id = self.speaker_id.view(batch_size * seq_len).long()
+        self.finder_loss = self.f_criterion(pred_speaker_id, real_speaker_id)
         self.log('finder_loss', self.finder_loss, )
 
     def backward_G(self):
@@ -81,13 +85,13 @@ class HFC(L.LightningModule):
 
         # newly updated finder can generate a better training signal
         # Softmax converts to probabilities, which we can use for leakage loss
-        self.pred_cv = F.softmax(self.finder(self.hidden), dim=2)
+        self.pred_speaker_id = F.softmax(self.finder(self.hidden), dim=2)
 
         # Use output from forward earlier to do mse between resynthed, controlled output and
         # ground truth input
         self.combiner_loss = self.g_criterion(self.controlled, self.x)
 
-        self.leakage_loss = torch.var(self.pred_cv, 2)
+        self.leakage_loss = torch.var(self.pred_speaker_id, 2)
         # Use mean to reduce along all timesteps
         self.leakage_loss = self.leakage_loss.mean()
         self.g_losses = self.combiner_loss + self.hp.beta * self.leakage_loss
@@ -127,15 +131,13 @@ class HFC(L.LightningModule):
 
 
     def anneal(self):
+        # TODO replace with LR scheduler
         self.hp.g_lr *= self.hp.annealing_rate
         self.hp.f_lr *= self.hp.annealing_rate
         print('Annealing', self.hp.f_lr, self.hp.g_lr)
         generator_params = list(self.hider.parameters()) + list(self.combiner.parameters())
-        self.g_opt = torch.optim.Adam(generator_params, lr=self.hp.g_lr, weight_decay=0.00)
-        self.f_opt = torch.optim.Adam(self.finder.parameters(), lr=self.hp.f_lr, weight_decay=0.00)
+        self.g_opt = Adam(generator_params, lr=self.hp.g_lr, weight_decay=0.00)
+        self.f_opt = Adam(self.finder.parameters(), lr=self.hp.f_lr, weight_decay=0.00)
 
-    def save_nets(self):
-        # Save network params to disk
-        pass
 
 
