59ebe3144ca801b7d21b33d27849696f6286e05b
Jacob J Webber <jacobjwebber@gmail.com>
2023-12-23 12:15:04 +0000

    tasty bowl of plottage

---
+++ plottage.py
@@ -0,0 +1,23 @@
+from scipy.signal import spectrogram
+import holoviews as hv
+import panel as pn
+from scipy.io import wavfile
+import numpy as np
+from bokeh.resources import INLINE
+
+hv.extension("bokeh", logo=False)
+
+# from https://docs.wandb.ai/guides/track/log/plots
+def save_audio_with_bokeh_plot_to_html(audio_path, html_file_name):
+    sr, wav_data = wavfile.read(audio_path)
+    duration = len(wav_data) / sr
+    f, t, sxx = spectrogram(wav_data, sr)
+    spec_gram = hv.Image((t, f, np.log10(sxx)), ["Time (s)", "Frequency (hz)"]).opts(
+        width=500, height=150, labelled=[]
+    )
+    audio = pn.pane.Audio(wav_data, sample_rate=sr, name="Audio", throttle=500)
+    slider = pn.widgets.FloatSlider(end=duration, visible=False)
+    line = hv.VLine(0).opts(color="white")
+    slider.jslink(audio, value="time", bidirectional=True)
+    slider.jslink(line, value="glyph.location")
+    combined = pn.Row(audio, spec_gram * line, slider).save(html_file_name)
